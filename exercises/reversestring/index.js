dc9cf3023e51ecbee84f751c807a7ce54493ca11
Patrick TUNEZERWANE <tunezepatrick@gmail.com>
2021-02-08 12:30:03 +0200

    reverse

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,40 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+// // SOLUTION 1
+// function reverse(str) {
+//     const arr = str.split(''); // MAKE THE STRING INTO AN ARRAY SO THAT WE CAN BE ABLE TO USE REVERSE BUILT-IN FUNC
+//     arr.reverse(); // CALL REVERSE ON THE ARRAY JUST TO DO THE REVERSE
+//     return arr.join('') // JOIN BACK THE STRING
+// }
+
+// module.exports = reverse;
+
+
+// // SOLUTION 2
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// module.exports = reverse;
+
+// SOLUTION 3
+// function reverse(str) {
+//     let reversedSTR = '';
+//     for (let char of str){
+//         reversedSTR = char + reversedSTR;
+//     }
+
+//     return reversedSTR
+// }
+
+// module.exports = reverse;
+
+
+// SOLUTION 4
+function reverse(str) {
+    return str.split('').reduce((rev, chr) => chr + rev, '' ) // learn how reduce works
+}
 
 module.exports = reverse;

6ded16eafc931ba2a856b703506274d8a566273f
Patrick TUNEZERWANE <tunezepatrick@gmail.com>
2021-02-08 17:17:25 +0200

    reverse an integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,25 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+
+//SOLUTION ONE
+// function reverseInt(n) {
+//     const reversed = n.toString().split('').reverse().join(''); // reversing a num we needed first to convert it into a string
+//     if (n < 0 ){
+//         return parseInt(reversed) * -1 // here because this line of code (const reversed = n.toString().split('').reverse().join('');) alwas gives us postive num so we have to multiply by -1 for negative nums
+//     } 
+//     return parseInt(reversed)
+// }
+
+// module.exports = reverseInt;
+
+
+//SOLUTION TWO (EVEN PERFECT THAN THE ONE ABOVE)
+function reverseInt(n) {
+    const reversed = n.toString().split('').reverse().join(''); // reversing a num we needed first to convert it into a string
+    // use sign on Math instead of the if condition// learn more about how Math.sign() works here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sign
+    return parseInt(reversed) * Math.sign(n)
+}
 
 module.exports = reverseInt;
+

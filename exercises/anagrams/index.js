6069982eb089226938f2f84e1859bfce3b9daa3e
Patrick TUNEZERWANE <tunezepatrick@gmail.com>
2021-02-13 13:32:20 +0200

    anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,15 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    return cleanStringUp(stringA) === cleanStringUp(stringB)
+}
+
+function cleanStringUp(str){
+    // replace() removes pancutuations and spaces
+    // split() makes a string into an array, and we had to do it cuz sort() is an array method
+    // join() takes back an arry back to string
+    return str.replace(/[^\w]/g, '').split('').sort().join('');
+}
 
 module.exports = anagrams;
